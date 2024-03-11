a069e8c05e3154c8285b75fd4d819dddef20086b
joshc3453 <joshc3453@gmail.com>
2024-03-10 20:43:47 -0400

    removing 5 slice limit

--- player_logs.py
+++ player_logs.py
@@ -11,10 +11,10 @@ def get_nba_player_stats(season='2023-24', max_retries=5):
     nba_players = players.get_players()
 
     # Get players that are active
-    active_nba_players = [player for player in nba_players if player['is_active'] == True][:5]
+    active_nba_players = [player for player in nba_players if player['is_active'] == True]
 
     # Get a list of the player IDs for active players
-    active_nba_player_ids = [player['id'] for player in active_nba_players][:5]
+    active_nba_player_ids = [player['id'] for player in active_nba_players]
 
     while max_retries > 0:
         try:
